421cfcc5156c8f05dbc9a283f6079455c95824a8
unique <zhangel10@gmail.com>
2017-12-08 21:40:25 +0800

    FSTSerializer

--- src/main/java/com/unique/bullet/serializer/FSTSerializer.java
+++ src/main/java/com/unique/bullet/serializer/FSTSerializer.java
@@ -1,8 +1,8 @@
 package com.unique.bullet.serializer;
 
-import de.ruedigermoeller.serialization.FSTConfiguration;
-
 import java.io.Serializable;
+
+import de.ruedigermoeller.serialization.FSTConfiguration;
 
 public class FSTSerializer implements ISerializer {
     private final static FSTConfiguration fst = FSTConfiguration.createDefaultConfiguration();
@@ -18,7 +18,6 @@ public class FSTSerializer implements ISerializer {
         return FSTSerializer.FSTHolder.INSTANCE;
     }
 
-    @Override
     public byte[] serialize(Serializable obj) throws Exception {
         if (obj == null) {
             return null;
@@ -26,7 +25,6 @@ public class FSTSerializer implements ISerializer {
         return fst.asByteArray(obj);
     }
 
-    @Override
     public <T> T deserialize(byte[] bytes) throws Exception {
         if (bytes == null || bytes.length <= 0) {
             return null;
